bfae8d7079c8e7b3c1b226831e6d9539acdb4735
axm@example.com <axm@example.com>
2018-05-25 20:27:54 +0200

    consolidate url parsing calling code

--- src/Network/CrawlChain/Crawling.hs
+++ src/Network/CrawlChain/Crawling.hs
@@ -11,11 +11,12 @@ import qualified Data.ByteString.Char8 as BC
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Network.Http.Client as C
-import Network.URI (URI (..), parseURI, escapeURIString, isUnescapedInURI)
+import Network.URI (URI(..))
 
 import Network.CrawlChain.CrawlAction
 import Network.CrawlChain.CrawlResult
 import Network.CrawlChain.Util
+import Network.URI.Util
 
 type Crawler = CrawlAction -> IO CrawlResult
 type CrawlActionDescriber = CrawlAction -> String
@@ -81,11 +82,7 @@ ajaxRequest = postRequest C.concatHandler ajaxRequestChanges where
     where
       u = parseURL url where
         parseURL :: C.URL -> URI
-        parseURL r' =  -- TODO use Network.URI.Util to have only one piece of code doing this
-          case parseURI r of
-          Just u'  -> u'
-          Nothing -> error ("Can't parse URI - FIXME Crawling?: " ++ r)
-          where r = escapeURIString isUnescapedInURI $ T.unpack $ T.decodeUtf8 r'
+        parseURL = toURI . T.unpack . T.decodeUtf8
       q = C.buildRequest1 $ do
         C.http C.POST (path u)
         C.setAccept $ BC.pack "*/*"
